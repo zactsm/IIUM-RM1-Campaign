208377dd31c6e0c2e6af61758791f6c337d19fe4
zactsm <121001048+zactsm@users.noreply.github.com>
2023-11-26 00:55:03 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const scrollToFormButton = document.getElementById('scrollToForm');
+const scrollToContactButton = document.getElementById('scrollToContact');
+const formSection = document.querySelector('.form-section');
+const contactSection = document.querySelector('.contact-info');
+
+function scrollToSection(section) {
+  window.scrollTo({
+    top: section.offsetTop,
+    behavior: 'smooth'
+  });
+}
+
+scrollToFormButton.addEventListener('click', function() {
+  scrollToSection(formSection);
+});
+
+scrollToContactButton.addEventListener('click', function() {
+  scrollToSection(contactSection);
+});
